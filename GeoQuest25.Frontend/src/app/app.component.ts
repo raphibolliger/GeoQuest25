adf306f142a726d600976b7f5dd118e8b6aae56f
Raphael Bolliger <raphael@yarx.ch>
2025-02-22 21:12:22 +0100

    wip

--- GeoQuest25.Frontend/src/app/app.component.ts
+++ GeoQuest25.Frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [CommonModule, NgxMapboxGLModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'GeoQuest25';
+  readonly #httpClient = inject(HttpClient);
+
+  #geoJsonSub = this.#httpClient.get<GeoJSON.FeatureCollection>('/assets/switzerland-municipalities-234523.geojson').subscribe((data) => {
+    console.log(data);
+    this.geoJson.set(data);
+  });
+
+  geoJson = signal<GeoJSON.FeatureCollection | undefined>(undefined);
+
+  paint = signal<mapboxgl.FillPaint>({
+    'fill-color': '#088',
+    'fill-opacity': 0.8,
+  });
 }
--- GeoQuest25.Frontend/src/app/app.config.ts
+++ GeoQuest25.Frontend/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom([
+      NgxMapboxGLModule.withConfig({
+        accessToken: 'pk.eyJ1IjoicmFwaGlib2xsaWdlciIsImEiOiJjbG1hOXF6d3cwOGtmM2ZzZzVqbHdlYzdpIn0.1ig4AVATbPkclRMLN-B_WQ', // Optional, can also be set per map (accessToken input of mgl-map)
+      }),
+    ]),
+  ],
 };

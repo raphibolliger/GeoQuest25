e970933831e691c6500bd584fce3cac62e16500f
Raphael Bolliger <raphael@yarx.ch>
2025-03-09 21:44:51 +0100

    auto replace new files in frontend app

--- GeoQuest25.Processing/GeoQuest25.Processing/Program.cs
+++ GeoQuest25.Processing/GeoQuest25.Processing/Program.cs
@@ -37,8 +37,35 @@ Parallel.ForEach(municipalities, municipality =>
     }
 });
 
-await GenerateGeoJson(visited.ToArray(), $"/Users/raphi/Downloads/visited-{Guid.NewGuid()}.geojson");
-await GenerateGeoJson(todo.ToArray(), $"/Users/raphi/Downloads/todo-{Guid.NewGuid()}.geojson");
+// delete old geojson files and remove old ones
+var frontendPaht = SearchFrontendPath();
+if (frontendPaht is null)
+    throw new ApplicationException("Frontend assets path is null. Updating geojson files not possible.");
+
+var assetsPath = Path.Combine(frontendPaht, "src/assets");
+var geojsonAssetFiles = Directory.GetFiles(assetsPath, "*.geojson");
+
+var existingVisitedFilePath = geojsonAssetFiles.Single(f => f.Contains("visited-"));
+var existingVisitedFile = new FileInfo(existingVisitedFilePath);
+File.Delete(existingVisitedFilePath);
+
+var existingTodoFilePath = geojsonAssetFiles.Single(f => f.Contains("todo-"));
+var existingTodoFile = new FileInfo(existingTodoFilePath);
+File.Delete(existingTodoFilePath);
+
+var newVisitedFileName = $"visited-{Guid.NewGuid()}.geojson";
+await GenerateGeoJson(visited.ToArray(), $"{assetsPath}/{newVisitedFileName}");
+
+var newTodoFileName = $"todo-{Guid.NewGuid()}.geojson";
+await GenerateGeoJson(todo.ToArray(), $"{assetsPath}/{newTodoFileName}");
+
+// replace geosjon reference in app.component.ts
+var appComponentPath = Path.Combine(frontendPaht, "src/app/app.component.ts");
+
+var appComponentContent = await File.ReadAllTextAsync(appComponentPath);
+var newAppComponentContent = appComponentContent.Replace(existingVisitedFile.Name, newVisitedFileName).Replace(existingTodoFile.Name, newTodoFileName);
+
+await File.WriteAllTextAsync(appComponentPath, newAppComponentContent);
 
 return;
 
@@ -81,4 +108,22 @@ static async Task GenerateGeoJson(Municipality[] municipalities, string outputPa
     // In Datei schreiben
     await File.WriteAllTextAsync(outputPath, geoJsonString);
     Console.WriteLine($"GeoJSON wurde nach {outputPath} geschrieben.");
+}
+
+string? SearchFrontendPath()
+{
+    var currentDirectory = Directory.GetCurrentDirectory();
+    while (currentDirectory is not null)
+    {
+        var direcotryInfo = new DirectoryInfo(currentDirectory);
+        var directories = direcotryInfo.GetDirectories();
+        if (directories.Any(d => d.Name == "GeoQuest25.Frontend"))
+        {
+            var path = Path.Combine(currentDirectory, "GeoQuest25.Frontend");
+            return Directory.Exists(path) ? path : null;
+        }
+        
+        currentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+    }
+    return currentDirectory;
 }

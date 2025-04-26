4043bd352da3032252997827c48df1e75e9b82c5
Raphael Bolliger <raphael@yarx.ch>
2025-04-27 00:37:37 +0200

    refactor: enhance keyboard event handling and improve signal management

--- GeoQuest25.Frontend/src/app/app.component.ts
+++ GeoQuest25.Frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { httpResource } from '@angular/common/http';
-import { Component, computed, effect, resource, signal } from '@angular/core';
+import { Component, computed, linkedSignal, resource, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Expression } from 'mapbox-gl';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { fromEvent } from 'rxjs';
+
+const markerIcon =
+  'data:image/svg+xml;charset=utf-8,' +
+  encodeURIComponent(`
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+      <circle cx="12" cy="12" r="8" fill="#4285F4" stroke="white" stroke-width="3"/>
+    </svg>
+  `);
 
 @Component({
   selector: 'app-root',
@@ -13,7 +21,46 @@ import { fromEvent } from 'rxjs';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  readonly mapStyleSelection = signal<'light' | 'streets' | 'satellite'>('light');
+  // consts
+  readonly markerIcon = markerIcon;
+
+  // keyboard events
+  readonly #keydown = toSignal(fromEvent<KeyboardEvent>(document, 'keydown'));
+
+  // signals
+  readonly linePaint = signal<mapboxgl.LinePaint>({ 'line-color': '#000000', 'line-width': 1 });
+  readonly selectedMunicipality = signal<{ name: string; firstVisit: string | undefined } | undefined>(undefined);
+  readonly showPosition = signal(false);
+
+  // linked signals
+  readonly mapStyleSelection = linkedSignal<KeyboardEvent | undefined, 'light' | 'streets' | 'satellite'>({
+    source: this.#keydown,
+    computation: (keydown, previous) => {
+      if (keydown?.ctrlKey) {
+        if (keydown.key === '1') {
+          return 'light';
+        } else if (keydown.key === '2') {
+          return 'streets';
+        } else if (keydown.key === '3') {
+          return 'satellite';
+        }
+      }
+      return previous?.value ?? 'light';
+    },
+  });
+
+  readonly #showTransparent = linkedSignal<KeyboardEvent | undefined, boolean>({
+    source: this.#keydown,
+    computation: (keydown, previous) => {
+      // if ctrl + h is pressed, toggle the hideTodo signal
+      if (keydown?.ctrlKey && keydown.key === 'h') {
+        return !previous?.value;
+      }
+      return previous?.value ?? false;
+    },
+  });
+
+  // computed
   readonly mapStyle = computed(() => {
     switch (this.mapStyleSelection()) {
       case 'streets':
@@ -25,44 +72,16 @@ export class AppComponent {
     }
   });
 
-  // keyboard events
-  readonly keydown = toSignal(fromEvent<KeyboardEvent>(document, 'keydown'));
-
-  readonly keydownEffect = effect(() => {
-    const key = this.keydown();
-    if (key?.ctrlKey) {
-      if (key.key === '1') {
-        this.mapStyleSelection.set('light');
-      } else if (key.key === '2') {
-        this.mapStyleSelection.set('streets');
-      } else if (key.key === '3') {
-        this.mapStyleSelection.set('satellite');
-      } else if (key.key === 'h') {
-        this.hideTodo.update((prev) => !prev);
-      }
-    }
-  });
-
-  readonly visitedData = httpResource<GeoJSON.FeatureCollection>('./assets/visited-40a6218f-a54c-4500-8f01-ccdf3f8a041b.geojson');
-  readonly todoData = httpResource<GeoJSON.FeatureCollection>('./assets/todo-0e326a0e-3376-42ec-a895-0ab406533b80.geojson');
-
   readonly visitedCount = computed(() => this.visitedData.value()?.features.length);
   readonly todoCount = computed(() => this.todoData.value()?.features.length);
   readonly totalCount = computed(() => (this.visitedCount() ?? 0) + (this.todoCount() ?? 0));
-
-  readonly hideTodo = signal(false);
-  readonly selectedMunicipality = signal<{ name: string; firstVisit: string | undefined } | undefined>(undefined);
-
-  linePaint = signal<mapboxgl.LinePaint>({ 'line-color': '#000000', 'line-width': 1 });
-
-  visitedPaint = computed<mapboxgl.FillPaint>(() => {
-    const hideTodo = this.hideTodo();
+  readonly visitedPaint = computed<mapboxgl.FillPaint>(() => {
+    const showTransparent = this.#showTransparent();
     const fillExpression: Expression = ['case', ['==', ['get', 'name'], this.selectedMunicipality()?.name ?? ''], '#FFFF00', '#0000FF'];
-    return { 'fill-color': fillExpression, 'fill-opacity': hideTodo ? 0.2 : 0.5 };
+    return { 'fill-color': fillExpression, 'fill-opacity': showTransparent ? 0.2 : 0.5 };
   });
-
-  todoPaint = computed<mapboxgl.FillPaint>(() => {
-    const hideTodo = this.hideTodo();
+  readonly todoPaint = computed<mapboxgl.FillPaint>(() => {
+    const showTransparent = this.#showTransparent();
     const fillExpression: Expression = [
       'case',
       // set to red if selected
@@ -72,29 +91,16 @@ export class AppComponent {
       ['==', ['get', 'planned'], true],
       '#00FF00',
       // fallback to white or transparent if not visited and not planned
-      hideTodo ? 'transparent' : '#FFFFFF',
+      showTransparent ? 'transparent' : '#FFFFFF',
     ];
-    return { 'fill-color': fillExpression, 'fill-opacity': hideTodo ? 0.2 : 0.5 };
+    return { 'fill-color': fillExpression, 'fill-opacity': showTransparent ? 0.2 : 0.5 };
   });
 
-  layerClick($event: mapboxgl.MapMouseEvent & { features?: mapboxgl.MapboxGeoJSONFeature[] | undefined } & mapboxgl.EventData): void {
-    const properties = getProperties($event.features?.[0].properties);
-    this.selectedMunicipality.update((prev) => (prev?.name === properties?.name ? undefined : properties));
-  }
-
-  readonly markerIcon =
-    'data:image/svg+xml;charset=utf-8,' +
-    encodeURIComponent(`
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-      <circle cx="12" cy="12" r="8" fill="#4285F4" stroke="white" stroke-width="3"/>
-    </svg>
-  `);
-
-  geoPermissionStatus = resource({ loader: () => navigator.permissions.query({ name: 'geolocation' }) });
-
-  showPosition = signal(false);
-
-  position = resource({
+  // resources
+  readonly visitedData = httpResource<GeoJSON.FeatureCollection>('./assets/visited-40a6218f-a54c-4500-8f01-ccdf3f8a041b.geojson');
+  readonly todoData = httpResource<GeoJSON.FeatureCollection>('./assets/todo-0e326a0e-3376-42ec-a895-0ab406533b80.geojson');
+  readonly geoPermissionStatus = resource({ loader: () => navigator.permissions.query({ name: 'geolocation' }) });
+  readonly position = resource({
     request: () => this.showPosition(),
     loader: (request) => {
       if (request.request) {
@@ -109,6 +115,12 @@ export class AppComponent {
       }
     },
   });
+
+  // view actions
+  layerClick($event: mapboxgl.MapMouseEvent & { features?: mapboxgl.MapboxGeoJSONFeature[] | undefined } & mapboxgl.EventData): void {
+    const properties = getProperties($event.features?.[0].properties);
+    this.selectedMunicipality.update((prev) => (prev?.name === properties?.name ? undefined : properties));
+  }
 
   toggleLocation(): void {
     this.showPosition.update((prev) => !prev);
